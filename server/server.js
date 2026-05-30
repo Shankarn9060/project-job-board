c8251f47f387dd5dca134df876818c5e6f0dbdba
Uma Shankar <97588329+Shankarn9060@users.noreply.github.com>
2026-05-31 00:29:03 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-const jobsRoutes = require("./routes/jobsroutes.js");
+const jobsRoutes = require("./routes/jobroute.js");
 
 const app = express();
 
