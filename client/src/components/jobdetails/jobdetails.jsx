501ce623ec5cb7da1ac5519bbb19af32724efc24
Uma Shankar <97588329+Shankarn9060@users.noreply.github.com>
2026-05-30 22:57:49 +0530

    Update jobdetails.jsx

--- client/src/components/jobdetails/jobdetails.jsx
+++ client/src/components/jobdetails/jobdetails.jsx
@@ -1,77 +1,55 @@
-// pages/JobDetails.js
-
 import { useEffect, useState } from "react";
-
 import { useParams } from "react-router-dom";
 
 import API from "../api/api.jsx";
-
 import Navbar from "../navbar/navbar.jsx";
 import Footer from "../footer/footer.jsx";
 
 function JobDetails() {
-
   // Get job id from URL
   const { id } = useParams();
 
   // Store single job data
   const [job, setJob] = useState(null);
 
+  // Fetch job when page loads
+  useEffect(() => {
+    const fetchJob = async () => {
+      try {
+        const res = await API.get(`/jobs/${id}`);
+        setJob(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
-
-useEffect(() => {
-  const fetchJob = async () => {
-    try {
-      const res = await API.get(`/jobs/${id}`);
-      setJob(res.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  fetchJob();
-}, [id]);
-
-
-
+    fetchJob();
+  }, [id]);
 
   // Loading State
   if (!job) {
-
     return (
       <h3 className="text-center mt-5">
         Loading...
       </h3>
     );
-
   }
 
-
-
-
   return (
-
     <>
-
       <Navbar />
 
-
-
       <div className="container my-5">
-
         <div className="card job-details-card p-4">
-
-            <img
+          <img
             src={job.image}
             alt={job.title}
             className="job-details-img mb-4"
-            />
+          />
 
           <h2 className="text-primary fw-bold mb-3">
             {job.title}
           </h2>
-
-
 
           <p>
             <strong>Company:</strong> {job.company}
@@ -85,11 +63,7 @@ useEffect(() => {
             <strong>Salary:</strong> {job.salary}
           </p>
 
-
-
           <hr />
-
-
 
           <h5 className="fw-bold">
             Job Description
@@ -99,22 +73,14 @@ useEffect(() => {
             {job.description}
           </p>
 
-
-
           <button className="btn btn-teal mt-3">
             Apply Now
           </button>
-
         </div>
-
       </div>
 
-
-
       <Footer />
-
     </>
-
   );
 }
 
