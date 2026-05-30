e33e1be147550313cd7b43744a2fd7bc7025ae11
Uma Shankar <97588329+Shankarn9060@users.noreply.github.com>
2026-05-31 00:11:08 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,20 @@
-const PORT = process.env.PORT || 5000;
+const express = require("express");
+const cors = require("cors");
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+const jobsRoutes = require("./routes/jobsRoutes.js");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Job Board API Running"
+  });
 });
+
+app.use("/api/jobs", jobsRoutes);
+
+module.exports = app;
