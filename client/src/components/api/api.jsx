4a8999b0b137e076e0dbbb48b46fc4dd663afebc
Uma Shankar <97588329+Shankarn9060@users.noreply.github.com>
2026-05-31 00:50:56 +0530

    Update api.jsx

--- client/src/components/api/api.jsx
+++ client/src/components/api/api.jsx
@@ -1,13 +1,7 @@
 import axios from "axios";
 
-
-
 const API = axios.create({
-
-  baseURL: "https://your-backend.onrender.com/api"
-
+  baseURL: "https://project-job-board-mu.vercel.app/api"
 });
-
-
 
 export default API;
